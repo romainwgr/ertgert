9e465a7bdfaf97b0790297863a148f40122190ef
WAGNER Romain <12203019@s30407.iutv.univ-paris13.fr>
2024-06-03 16:41:15 +0200

    age

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import {ref,reactive} from 'vue';
+  let nom=ref('Tom');
+  function touche(e){
+    nom.value=e.target.value;
+  }
+</script>
+
+<template>
+  <p>Le joueur {{nom}} a 500 points.</p>
+  <p>Le joueur {{nom}} a fini le niveau 3.</p>
+  <p>Le joueur {{nom}} est classé 4e.</p>
+  Nom: <input @keyup="touche" />
+</template>
+
+<style scoped>
+</style>
